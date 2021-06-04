d646c4382d2e7e4589b7ff24633a1c6ad313ec92
fumycat <fumycat@gmail.com>
2021-06-04 14:21:07 +0700

    test server working

--- server.py
+++ server.py
@@ -1,16 +1,22 @@
+import logging
 import asyncio
+
+import aioredis
 from aiohttp import web
 
+routes = web.RouteTableDef()
 
+@routes.get('/gemm')
 async def handle(request):
-    name = request.match_info.get('name', "Anonymous")
-    text = "Hello, " + name
-    await asyncio.sleep(4)
-    return web.Response(text=text)
+    idata = await request.json()
+    print(idata)
+    return web.json_response({'status': 'Ok', 'message': 'Message'})
+
 
 app = web.Application()
-app.add_routes([web.get('/', handle),
-                web.get('/{name}', handle)])
+app.add_routes(routes)
+
+logging.basicConfig(filename='server.log', level=logging.INFO)
 
 if __name__ == '__main__':
     web.run_app(app, port=6677)
